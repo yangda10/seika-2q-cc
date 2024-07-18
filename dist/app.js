31c901e2de5ac416f3ed8aa988843bbd8d1ed9bc
YangDayeon <dayeon102030@gmail.com>
2024-07-18 16:03:03 +0900

    -

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+new fullpage('#fullpage', {
+    sectionsColor: ['#A9A9F5', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    menu: '#menu',
+    navigation: true,
+    loopBottom: true,
+
+});
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBOztBQUVBLENBQUMsRSIsInNvdXJjZXMiOlsid2VicGFjazovL3B3ZC8uL3NyYy9hcHAuanMiXSwic291cmNlc0NvbnRlbnQiOlsibmV3IGZ1bGxwYWdlKCcjZnVsbHBhZ2UnLCB7XG4gICAgc2VjdGlvbnNDb2xvcjogWycjQTlBOUY1JywgJyM0QkJGQzMnLCAnIzdCQUFCRScsICd3aGl0ZXNtb2tlJ10sXG4gICAgYW5jaG9yczogWydmaXJzdFBhZ2UnLCAnc2Vjb25kUGFnZScsICd0aGlyZFBhZ2UnLCAnZm91cnRoUGFnZSddLFxuICAgIG1lbnU6ICcjbWVudScsXG4gICAgbmF2aWdhdGlvbjogdHJ1ZSxcbiAgICBsb29wQm90dG9tOiB0cnVlLFxuXG59KTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
