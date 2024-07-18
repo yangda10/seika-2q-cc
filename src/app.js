37293798110e5a2262da4d346c14b83c5dc29495
YangDayeon <dayeon102030@gmail.com>
2024-07-18 16:04:32 +0900

    commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    sectionsColor: ['#A9A9F5', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    menu: '#menu',
+    navigation: true,
+    loopBottom: true,
+
+});
