e6446d399fb0bf0e2b2d5ae4413c3082a903f2db
YangDayeon <dayeon102030@gmail.com>
2024-08-01 18:46:41 +0900

    commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  devtool: 'inline-source-map',
+  module: {
+      rules: [
+          {
+              test: /\.css$/i,
+              use: ["style-loader", "css-loader"],
+          },
+      ],
+  },
+  entry: {
+    main: './src/index.js',
+    app: './src/app.js',
+},
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist'
+}  
+};
